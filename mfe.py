17fa0298ba8c7028b3266049445417d809f625e5
Ron Kassay <Ron.Kassay@gmail.com>
2024-01-14 00:57:23 +0200

    changed: write face as it's detected added: age/gender indentification (disabled until fully fixed)

--- mfe.py
+++ mfe.py
@@ -2,16 +2,23 @@
 # coding: utf-8
 
 import os
+from pathlib import Path
+from pathlib import PurePath
 import argparse
 import cv2
 from matplotlib import pyplot as plt
 from deepface import DeepFace
 import numpy as np
-from moviepy.editor import *
 import math
 import time
 
 
+def classify(img):
+    res=DeepFace.analyze(img,actions=("age","gender"), enforce_detection = False, detector_backend='retinaface')
+    print(f"img={img} res={res}")
+    return {"age" : res[0]["age"], "gender":res[0]["dominant_gender"]}
+
+    
 def isFaceKnown(face, db):
     for index, known_face in enumerate(db):
         result = DeepFace.verify(img1_path = face, img2_path = known_face,
@@ -21,18 +28,21 @@ def isFaceKnown(face, db):
             return True
     return False
 
-def extract_faces(video_path,skip):
-    targetDir = f"{video_path}-faces"
+def extract_faces(videoPath,skip):
     sTime = time.time()
+
+    videoFileNameNoExt = Path(videoPath).stem
+    videoParentDir = PurePath(videoPath).parent
+    targetDir = os.path.join(videoParentDir,f"{videoFileNameNoExt}-faces")
 
     if not os.path.exists(targetDir):
         os.makedirs(targetDir)
 
     faces = []
-    cap = cv2.VideoCapture(video_path) # read video file
+    cap = cv2.VideoCapture(videoPath) # read video file
     fps = cap.get(cv2.CAP_PROP_FPS)
     fps = math.ceil(fps)
-    path = os.path.basename(video_path)
+    path = os.path.basename(videoPath)
     i = 0
 
     prevFrameFaces=[]
@@ -49,14 +59,16 @@ def extract_faces(video_path,skip):
                 continue
 
             face_props = DeepFace.extract_faces(img_path = frame,
-    #                                            target_size = (224, 224),
-                                               detector_backend = "retinaface",
-                                               enforce_detection = False
+                                             #  target_size = (524, 524),
+                                             #  detector_backend = "retinaface",
+                                               enforce_detection = False,
+                                               align = False
                                                )
             thisFrameFaces=[]
             for face_prop in face_props:
-                faceFrame  = cv2.cvtColor(face_prop['face'], cv2.COLOR_BGR2RGB)
-                faceFrame  = cv2.cvtColor(faceFrame, cv2.COLOR_BGR2RGB)
+                faceFrame  = face_prop['face']
+                #faceFrame  = cv2.cvtColor(face_prop['face'], cv2.COLOR_BGR2RGB)
+                #faceFrame  = cv2.cvtColor(faceFrame, cv2.COLOR_BGR2RGB)
                 confidence = face_prop['confidence']
                 if confidence > 0.998:
     #                 print(f"Face found: frame {i}: confidence={confidence}")
@@ -77,23 +89,35 @@ def extract_faces(video_path,skip):
                     thisFrameFaces.append(faceFrame)
                     if (not isFaceNew):
                         continue
-                    print(f"New face found: frame {i}: confidence={confidence}")
+                    # begin: disabled until fixed
+                    #cl=classify(faceFrame)
+                        
+                    #if (cl["gender"]=='Man'):
+                    #    gender="M"      
+                    #else:
+                    #    gender="F"
+                    #faceLabel=f"{gender}{len(faces)}_{cl['age']}yo"
+                    # end: disabled until fixed
+                    faceLabel=f"face_{len(faces)}"
+                    print(f"New face found: frame {i}: confidence={confidence} label={faceLabel}")
                     #plt.imshow(faceFrame)
                     #plt.show()
+                    writeFace(faceFrame,faceLabel,targetDir,videoFileNameNoExt)
                     faces.append(faceFrame)
                     
                 
-            prevFrameFaces=thisFrameFaces
+            prevFrameFaces = thisFrameFaces
             if (i % 10 == 0) and i > 1:
                 elapsed =  time.time()  - sTime
                 fps = i / elapsed
                 print(f"Total frames processed {i} faces={len(faces)} elapsed={time.strftime('%H:%M:%S', time.gmtime(elapsed))} fps={fps}")
                 
 
-    for index, known_face in enumerate(faces):
-        targetFilename = f'face_{index}.jpg'
+def writeFace(face, faceLabel ,targetDir, origin):
+
+        targetFilename=f"{faceLabel}_{origin}.png"
         outputPath = os.path.join(targetDir, targetFilename)
-        cv2.imwrite(outputPath, known_face[:, :, ::-1] * 255)
+        cv2.imwrite(outputPath, face[:, :, ::-1] * 255)
 
 def main(args):
 
